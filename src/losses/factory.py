e31e31c83b70be1c155c4b2e1243aa77765f3d33
koukyo1994 <arabiannight1994@yahoo.co.jp>
2020-02-17 09:02:42 +0100

    add focal loss

--- src/losses/factory.py
+++ src/losses/factory.py
@@ -1,4 +1,6 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from easydict import EasyDict as edict
 
@@ -39,6 +41,97 @@ class BengaliCrossEntropyLoss(nn.Module):
                 consonant_pred, consonant_true)
 
 
+class FocalLoss(nn.Module):
+    def __init__(self,
+                 alpha=1,
+                 gamma=2,
+                 logits=False,
+                 reduction='elementwise_mean'):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.logits = logits
+        self.reduction = reduction
+
+    def forward(self, inputs, targets):
+        if self.logits:
+            BCE_loss = F.binary_cross_entropy_with_logits(
+                inputs, targets, reduction='none')
+        else:
+            BCE_loss = F.binary_cross_entropy(
+                inputs, targets, reduction='none')
+        pt = torch.exp(-BCE_loss)
+        F_loss = self.alpha * (1 - pt)**self.gamma * BCE_loss
+
+        if self.reduction is None:
+            return F_loss
+        else:
+            return torch.mean(F_loss)
+
+
+class MultiHingeLoss(nn.Module):
+    def __init__(self, p=1, margin=1, weight=None, size_average=True):
+        super().__init__()
+        self.p = p
+        self.margin = margin
+        self.weight = weight
+        self.size_average = size_average
+
+    def forward(self, output, y):
+        output_y = output[torch.arange(0,
+                                       y.size()[0]).long().cuda(),
+                          y.data.cuda()].view(-1, 1)
+        loss = output - output_y + self.margin
+        loss[torch.arange(0, y.size()[0]).long().cuda(), y.data.cuda()] = 0
+        loss[loss < 0] = 0
+        if (self.p != 1):
+            loss = torch.pow(loss, self.p)
+
+        if (self.weight is not None):
+            loss = loss * self.weight
+
+        loss = torch.sum(loss)
+        if (self.size_average):
+            loss /= output.size()[0]
+        return loss
+
+
+class BengaliFocalLoss(nn.Module):
+    def __init__(self,
+                 n_grapheme: int,
+                 n_vowel: int,
+                 n_consonant: int,
+                 weights=(1.0, 1.0, 1.0)):
+        super().__init__()
+        self.n_grapheme = n_grapheme
+        self.n_vowel = n_vowel
+        self.n_consonant = n_consonant
+        self.focal = FocalLoss()
+        self.weights = weights
+
+    def forward(self, pred, true):
+        head = 0
+        tail = self.n_grapheme
+        grapheme_pred = pred[:, head:tail]
+        grapheme_true = true[:, 0]
+
+        head = tail
+        tail = head + self.n_vowel
+        vowel_pred = pred[:, head:tail]
+        vowel_true = true[:, 1]
+
+        head = tail
+        tail = head + self.n_consonant
+        consonant_pred = pred[:, head:tail]
+        consonant_true = true[:, 2]
+
+        return self.weights[0] * self.focal(
+            grapheme_pred, grapheme_true) + \
+            self.weights[1] * self.focal(vowel_pred, vowel_true) + \
+            self.weights[2] * self.focal(
+                consonant_pred, consonant_true)
+
+
 class BengaliMultiMarginLoss(nn.Module):
     def __init__(self,
                  n_grapheme: int,
@@ -49,7 +142,7 @@ class BengaliMultiMarginLoss(nn.Module):
         self.n_grapheme = n_grapheme
         self.n_vowel = n_vowel
         self.n_consonant = n_consonant
-        self.margin = nn.MultiMarginLoss()
+        self.margin = MultiHingeLoss()
         self.weights = weights
 
     def forward(self, pred, true):
@@ -123,7 +216,9 @@ def get_loss(config: edict):
     elif name == "grapheme":
         criterion = GraphemeLoss()  # type: ignore
     elif name == "margin":
-        criterion = BengaliMultiMarginLoss()  # type: ignore
+        criterion = BengaliMultiMarginLoss(**params)  # type: ignore
+    elif name == "focal":
+        criterion = BengaliFocalLoss(**params)  # type: ignore
     else:
         raise NotImplementedError
     return criterion
